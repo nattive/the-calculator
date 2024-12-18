f685acc934f0a0c3d9c84c9b652160dfd25d46b5
Ademola <ademola.sofolahan@alerzo.com>
2024-12-18 17:38:48 +0100

    Update ResultPage.tsx

--- src/app/components/frames/ResultPage.tsx
+++ src/app/components/frames/ResultPage.tsx
@@ -24,17 +24,18 @@ const ResultPage = ({
     country,
     nights,
     cost,
+    careType,
     yearlyProjectedSavings
 }: Props) => {
     return (
         <main className="max-w-3xl mx-auto p-4 sm:p-6 mt-4 sm:mt-8 space-y-8">
             {/* Savings Card */}
             <div className="bg-[#E8FAE9] rounded-lg p-8 text-center">
-                <Typography variant='h3'>With TrustedHousesitters you could save</Typography>
-                <Typography variant='display'>{country?.currency}{(Math.ceil(yearlyProjectedSavings)).toLocaleString()} a year</Typography>
+                <Typography className='text-primary-900' variant='h3'>With TrustedHousesitters you could save</Typography>
+                <Typography className='text-primary-900' variant='display'>{country?.currency}{(Math.ceil(yearlyProjectedSavings)).toLocaleString()} a year</Typography>
 
                 <Typography variant='body' className='font-normal'>
-                    based on {nights} nights of pet care for a {petType.toLowerCase()} in {location ? `${location},` : null}, {country?.name}
+                    based on {nights} nights of pet care for a {petType.toLowerCase()} in {location ? `${location},` : null} {country?.name}
                 </Typography>
             </div>
             {/* Cost Breakdown */}
@@ -42,13 +43,13 @@ const ResultPage = ({
                 <Typography variant='h3' className='text-primary-900 capitalize-first'>
                     <span className="font-medium"> You’ll pay </span>
                     <span className="font-bold capitalize-first"> {country?.currency}{Math.ceil(cost).toLocaleString()}</span>
-                    <span className="font-medium"> for dog sitting in </span>
+                    <span className="font-medium"> for {careType.toLowerCase()} in </span>
                     <span className="font-bold">{location ? `${location},` : null} {country?.name}</span>
                     <span className="font-medium"> But with TrustedHousesitters, you could save up to </span>
                     <span className="font-bold"> {country?.currency}{(Math.ceil(yearlyProjectedSavings)).toLocaleString()}</span>
                     <span className="font-medium"> for  </span>
                     <span className="font-bold"> {nights}</span>
-                    <span className="font-medium">day(s) </span>.
+                    <span className="font-medium">day(s)</span>.
                 </Typography>
             </div>
 
